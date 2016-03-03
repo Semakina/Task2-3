c1e4e2fc43d9f2c235557b397d4dd1a7f8b6e82e
Ольга Семакина <semakina.olga@gmail.com>
2016-03-03 19:12:58 +0300

    м

---
+++ ConsoleApplication12/Program.cs
@@ -0,0 +1,42 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int K;
+            K = Convert.ToInt32(Console.ReadLine());
+            string str = Console.ReadLine();
+            var results = str.Split(' ').Select(item => Convert.ToInt32(item)).ToArray();
+            int half = K / 2 + 1;
+            int[] halfres = new int[half];
+            for (int i = 0; i < half; i++)
+            {
+                halfres[i] = 10000;
+            }
+            for (int i = 0; i < K; i++)
+            {
+                for (int j = 0; j < half; j++)
+                {
+                    if (results[i]<halfres[j])
+                    {
+                        halfres[j] = results[i];
+                        break;
+                    }
+                }
+            }
+            int count = 0;
+            for (int i = 0; i < half; i++)
+            {
+                count += (halfres[i] / 2 + 1);
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
